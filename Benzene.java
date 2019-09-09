55f0bd42a0985d6d8c778649208eef1d192d3b7d
Dana <ngdana42@gmail.com>
2019-09-09 02:27:50 -0700

    Initial sharing of project

---
+++ Benzene.java
@@ -0,0 +1,22 @@
+import apcslib.*;
+public class Benzene
+{
+    public static void main(String[]args)
+    {
+        DrawingTool marker;
+        SketchPad pad;
+        pad = new SketchPad(600,600);
+        marker = new DrawingTool(pad);
+        marker.drawCircle(150);
+        marker.up();
+        marker.turnRight(90);
+        marker.move(200);
+        marker.down();
+        marker.turn(60);
+        for (int i = 1; i < 7; i++)
+        {
+            marker.turn(60);
+            marker.move(200);
+        }
+    }
+}
---
+++ House.java
@@ -0,0 +1,24 @@
+import apcslib.*;
+public class House
+{
+    public static void main(String[]args)
+    {
+        DrawingTool marker;
+        SketchPad pad;
+        pad = new SketchPad(600,600);
+        marker = new DrawingTool(pad);
+        marker.up();
+        marker.move(-150,-200);
+        marker.turnRight(143);
+        marker.move(300);
+        marker.down();
+        marker.turn(90);
+        for (int i = 1; i < 5; i++)
+        {
+            marker.turn(90);
+            marker.move(300);
+        }
+        marker.move(0,275);
+        marker.move(150,100);
+    }
+}
